b8619e628683190026b8b751a27d6013ee80b3b2
mlbors <matyas.bors@gmail.com>
2017-06-06 06:32:40 +0200

    Add SpritSheet class

---
+++ src/com/mlbors/mini_game/graphics/SpriteSheet.java
@@ -0,0 +1,72 @@
+package com.mlbors.mini_game.graphics;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+/**
+ * Mini Game - SprieSheet.java
+ * 
+ * @author MLBors
+ * @version 1.0.0.0
+ */
+
+public class SpriteSheet {
+	
+	/**********************/
+	/***** PROPERTIES *****/
+	/**********************/
+	
+	/*****/
+	/***** PUBLIC *****/
+	/*****/
+	
+	public int[] pixels;
+	
+	/*****/
+	/***** PRIVATE *****/
+	/*****/
+	
+	private String path;
+	private int size = 256;
+	
+	/****************************************/
+	/****************************************/
+	
+	/***********************/
+	/***** CONSTRUCTOR *****/
+	/***********************/
+	
+	public SpriteSheet(String path, int size) {
+		
+		this.path = path;
+		this.size = size;
+		pixels = new int[this.size * this.size];
+		load();
+	
+	}
+	
+	/****************************************/
+	/****************************************/
+	
+	/****************/
+	/***** LOAD *****/
+	/****************/
+	
+	private void load() {
+		
+		try {
+			
+			BufferedImage image = ImageIO.read(SpriteSheet.class.getResource(path));
+			int w = image.getWidth();
+			int h = image.getHeight();
+			image.getRGB(0, 0, w, h, pixels, 0, w);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
